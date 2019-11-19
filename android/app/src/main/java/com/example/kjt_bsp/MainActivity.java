7a0bb6852c6f313709da5b4c04765b8ea97abebc
MinYa <1049468118@qq.com>
2019-11-19 09:24:36 +0800

    上个提交方法有问题，会导致在任何一个页面都是直接最小化应用，因此回退更正

--- android/app/src/main/java/com/example/kjt_bsp/MainActivity.java
+++ android/app/src/main/java/com/example/kjt_bsp/MainActivity.java
@@ -4,27 +4,10 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-import android.view.KeyEvent;
-
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
-    }
-
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            moveTaskToBack(false);
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-    
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(false);
-        super.onBackPressed();
-    
     }
 }
