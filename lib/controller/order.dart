0dcb2d00af264b38f09565b0c5f6bf49fafb41c8
MinYa <1049468118@qq.com>
2019-11-27 10:19:21 +0800

    刷新组件自己管理数据源

---
+++ lib/controller/order.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:kjt_bsp/common/httpRequest.dart';
+import 'package:kjt_bsp/config/apiConfig.dart';
+
+Future getOrderList(pageIndex, callback) async{
+    await HttpRequest.postWithoutToken(ApiConfig.orderList, {
+        'pageIndex': pageIndex
+    }, (result) {
+        callback(json.decode(json.encode(result['data'])), json.decode(json.encode(result['noMore'])));
+    });
+}
